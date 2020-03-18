908e5400e0670f22300c7260747b2177624dd811
robert <robert.apikyan@volo.global>
2020-03-18 07:26:16 +0400

    1. Launcher programm added

---
+++ src/sample/toolkit/corellation/CrossCorrelation.java
@@ -0,0 +1,65 @@
+package sample.toolkit.corellation;
+
+import static sample.toolkit.corellation.Correlation.arrayShift;
+import static sample.toolkit.corellation.Correlation.value;
+
+public class CrossCorrelation {
+
+
+    public static double[] crossValues(
+            double[] sample,
+            double[] source,
+            double emptyValue,
+            double corrCoefficient
+    ) {
+        // Առաջին քայլով հավասարեցվում են տրված երկու զանգվածների երկարությունները
+
+        // Զանգված, որի երկարությունը հավասար է source զանգվածի երկարությանը,
+        // և պարունակելու է sample փոփոխականի արժեքները
+        double[] equalizedSample;
+
+        if (sample.length < source.length) {
+
+            equalizedSample = new double[source.length];
+
+            // equalizedSample փոփոխականը ստանում է
+            // sample փոփոխականի արժեքները,
+            // իսկ equalizedSample դատարկ դիրքերը լրացվում են
+            // emptyValue փոփոխականի արժեքներով
+            for (int i = 0; i < equalizedSample.length; i++) {
+                equalizedSample[i] = emptyValue;
+                if (sample.length > i) {
+                    equalizedSample[i] = sample[i];
+                }
+            }
+        } else {
+            equalizedSample = sample;
+        }
+
+        // Փոփոխական որի մեջ պահվելու են կորելյացիաի արժեքները
+        double[] corrValues = new double[sample.length + 2 * source.length];
+
+        int corrIndex = 0;
+        // Կատարվում է իտերացիա, որի յուրաքանչյուր քայլում կատարվում է
+        // առաջին sample զանգվածի տեղաշարժ երկրորդի նկատմամբ
+        for (int i = 1 - sample.length; i < sample.length + source.length + 1; i++) {
+            // Կատարվում է sample զանգվածի տեղաշարժ
+            double[] shiftedSample = arrayShiftLinear(equalizedSample, i, true);
+
+            // հաշվարկվում է կորելյացիայի արժեքը տեղաշարժած shiftedSample զանգվածի
+            // և source զանգվածի միջև
+            // իտերացիայի քայլի համար
+            corrValues[corrIndex] =
+                    value(0, Math.max(shiftedSample.length, source.length) - 1, shiftedSample, source) / corrCoefficient;
+
+            corrIndex++;
+        }
+
+        // Վերադարձնում է կորելյացիայի արժեքները
+        return corrValues;
+    }
+
+    public static double[] arrayShiftLinear(double[] array, int shift, boolean copyArray) {
+        return arrayShift(array, shift, false, copyArray, 0.0);
+    }
+}
